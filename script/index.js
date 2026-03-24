364ff1b89ae57d8f11c97de7c982aeec7bf83f15
Zhuoxuan C <zhuoxuan.chen00@gmail.com>
2026-03-24 20:01:55 +0100

    final

---
+++ script/index.js
@@ -0,0 +1,14 @@
+console.log('Hello, World!');
+
+let name = 'Jade';
+let age = 25;
+let myFavoriteColor = 'green';
+console.log(`I'm ${name}, I'm ${age} years old, and my favorite color is ${myFavoriteColor}.`);
+
+let x = 10;
+let y = 5;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
