5adba2ee18e398cf395b9560206c63d5e033e6bd
Zhuoxuan C <zhuoxuan.chen00@gmail.com>
2026-04-13 22:23:40 +0200

    added username as a parameter and argument

--- script/index-lesson3.js
+++ script/index-lesson3.js
@@ -1,6 +1,8 @@
-function isValidPassword(password) {
-    return password.lenth < 8 || password.includes(' ') || password.includes('username') ? false : true;
+function isValidPassword(password, username) {
+    return password.length < 8 || 
+    password.includes(' ') || 
+    password.includes(username) ? false : true;
 }
 
-console.log(isValidPassword("1122222222"));
-console.log(isValidPassword("1122 222222"));
+console.log(isValidPassword("1122222222", "john"));
+console.log(isValidPassword("1122 222222", "john"));
